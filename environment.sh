fa6c81a97a2b8696e6223a30772be7c8de317f31
wangjiaqi-lab-SYSU <63411445+wangjqspace@users.noreply.github.com>
2025-01-08 22:35:45 +0800

    Add files via upload

---
+++ environment.sh
@@ -0,0 +1,17 @@
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install pytorch-lightning==1.8.3
+pip install wandb
+pip install transformers==4.28.0
+pip install easydict
+pip install biopython
+pip install scipy
+pip install peft==0.10.0
+pip install lmdb
+pip install torchmetrics==0.9.3
+pip install pandas
+pip install fair-esm
+pip install numpy==1.24.4
+pip install datasets
+pip install scikit-learn
+pip install matplotlib
+pip install markupsafe
